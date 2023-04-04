254607ff36852ad8e86a592403c3f285703cb49f
Daniel <felipe9321@hotmail.com>
2023-04-03 20:08:09 -0500

    guardando proyecto

---
+++ src/Greeting.js
@@ -0,0 +1,8 @@
+export function Greeting() {
+
+     return <h1>componente de react</h1>;
+}
+
+export function UserCard(){
+    return <h1>Usuario tarjeta</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Greeting, UserCard} from "./Greeting"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<h1>hola Daniel</h1>);
 
-function Greeting() {
-  const user ={
 
-    firstName : "Daniel",
-    lastName : "Chica"
-
-
-  };
 
   
-}
+
 
 root.render(
-  <div>
+    <>
     <Greeting />
-  </div>
+    <UserCard/>
+    </>
 );
